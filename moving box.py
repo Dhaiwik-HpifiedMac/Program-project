bb41c3427b48eee26df19c7d431e077863512a2f
Dhaiwik-HpifiedMac <Dhaiwikm@gmail.com>
2025-03-22 20:16:08 -0700

    Update moving box.py

--- moving box.py
+++ moving box.py
@@ -1,4 +1,4 @@
-mport turtle
+import turtle
 t = turtle.Turtle()
 t.goto(0,0)
 screen = turtle.Screen()
@@ -12,19 +12,19 @@ def controls(intensity):
 
 
 def frame(x,y, xvel, yvel):
-   x = x + xvel
-   y = y + yvel
-   player(x,y,50)
+	x = x + xvel
+	y = y + yvel
+	player(x,y,50)
 
   
 def player(x,y,r):
 	t.pendown
-  t.goto(x-r,y-r)
+	t.goto(x-r,y-r)
 	t.forward(2*r)
 	t.right(2*r)
 	t.backward(2*r)
 	t.left(2*r)
-  t.penup
+	t.penup
 while playing = 0 
  #add code to be able to switch playing 
 while playing = 1
